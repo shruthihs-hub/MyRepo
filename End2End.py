2f1f7ebb8e6be48c4e65f39d5690a09fb78f9d5d
shruthihs-hub <shruthihs672@gmail.com>
2024-07-29 21:52:09 +0530

    My First Commit

---
+++ End2End.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
+driver.get("https://rahulshettyacademy.com/angularpractice/")
+driver.implicitly_wait(5)
+driver.maximize_window()
+
+driver.find_element(By.XPATH,"//a[@href='/angularpractice/shop']").click()
+items = driver.find_elements(By.XPATH,"//app-card/div[1]")
+for item in items:
+    tobeadd = item.find_element(By.XPATH,"div/h4").text
+    if tobeadd == "Blackberry":
+        item.find_element(By.XPATH,"div/button").click()
+
+driver.find_element(By.XPATH,"//a[@class='nav-link btn btn-primary']").click()
+
+driver .find_element(By.CSS_SELECTOR,".btn-success").click()
+
+driver.find_element(By.ID, "country").send_keys("Ind")
+wait = WebDriverWait(driver,10)
+wait.until(expected_conditions.presence_of_element_located((By.LINK_TEXT, "India")))
+driver.find_element(By.LINK_TEXT,"India").click()
+driver.find_element(By.XPATH,"//div[@class='checkbox checkbox-primary']").click()
+driver.find_element(By.XPATH, "//input[@type='submit']").click()
+message = driver.find_element(By.XPATH, "//div[@class='alert alert-success alert-dismissible']").text
+assert "Success! Thank you!" in message
---
+++ FormFill.py
@@ -0,0 +1,28 @@
+import time
+
+from select import select
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+#driver = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
+driver.get("https://rahulshettyacademy.com/angularpractice/")
+driver.maximize_window()
+time.sleep(5)
+driver.find_element(By.NAME, "name").send_keys("Shruthi H S")
+driver.find_element(By.CLASS_NAME, "ng-pristine").send_keys("demo113@gmail.com")
+driver.find_element(By.ID, "exampleInputPassword1").send_keys("123456")
+driver.find_element(By.XPATH, "//input[@id='exampleCheck1']").click()
+drop = Select(driver.find_element(By.ID, "exampleFormControlSelect1"))
+drop.select_by_visible_text("Male")
+drop.select_by_index(1)
+driver.find_element(By.CSS_SELECTOR,"#inlineRadio2").click()
+driver.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
+content = driver.find_element(By.CSS_SELECTOR, ".alert-success").text
+print(content)
+assert "Success" in content
+
+
